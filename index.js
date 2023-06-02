a39e9533db3fb6f18007b1b9817387dde984baef
Jonathan W. Zaleski <JonathanZaleski@gmail.com>
2023-06-02 17:57:07 -0500

    Auto-redirect to HTTPS in _Production_ + clean-up

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
-const express = require('express');
+import express from 'express';
 
 const host = process.env.HOST ?? 'localhost';
 const port = process.env.PORT ?? 3001;
 
 const app = express();
+
+app.use(function(req, res, next) {
+  if (process.env.NODE_ENV === 'production' && req.headers['x-forwarded-proto'] !== 'https') {
+    res.redirect(302, `https://${req.hostname}${req.originalUrl}`);
+  } else {
+    next();
+  }
+});
 
 app.use(express.static('public'));
 
